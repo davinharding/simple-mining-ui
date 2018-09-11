ebab1d00ed512f1e683d1781781fbcd394d0612d
Davin Harding <davinlharding@gmail.com>
2018-09-11 14:30:45 -0400

    Change index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/', (req, res) => {
+    res.end();
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
