f534518c449ed3c4c96c45954e99f1f1c137307a
rcurtis <visualsbycurtis@gmail.com>
2018-09-11 14:53:22 -0400

    Set up express server for static asset handling

--- index.js
+++ index.js
@@ -5,9 +5,5 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get('/', (req, res) => {
-    res.end();
-})
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
